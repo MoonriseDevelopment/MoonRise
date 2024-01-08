47893de23e689afd9e28ff4490eca868a90b4307
radmanplays <95340057+radmanplays@users.noreply.github.com>
2024-01-08 13:14:50 +0330

    add eclipse support + fix fly

--- src/main/java/dev/moonrise/module/movement/Fly.java
+++ src/main/java/dev/moonrise/module/movement/Fly.java
@@ -9,15 +9,20 @@ public class Fly extends Module {
     public Fly() {
         super("Fly", Keyboard.KEY_F, Category.MOVEMENT);
     }
-
+    public boolean isAlreadyEnabled;
+    
     public void onUpdate() {
         if(this.isToggled()) {
-        mc.thePlayer.capabilities.isFlying = true;
-        mc.thePlayer.capabilities.allowFlying = true;
+        	if (!isAlreadyEnabled) {
+		        mc.thePlayer.capabilities.isFlying = true;
+		        mc.thePlayer.capabilities.allowFlying = true;
+		        isAlreadyEnabled = true;
+        	} 
         }
     }
 
     public void onDisable() {
+    	isAlreadyEnabled = false;
         mc.thePlayer.capabilities.isFlying = false;
         mc.thePlayer.capabilities.allowFlying = false;
     }
